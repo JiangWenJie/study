3c1e49c48ec93bf14202e38f312f9da7de85327e
姜文杰 <wjjiang@che300.com>
2021-01-13 16:39:48 +0800

    fix:去除无效代码

--- algorithms/src/main/java/com/jwj/study/data/structure/hash/MyHashMap.java
+++ algorithms/src/main/java/com/jwj/study/data/structure/hash/MyHashMap.java
@@ -1,10 +1,5 @@
 package com.jwj.study.data.structure.hash;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
-
 /**
  * @author jwj
  * @date 2021/1/6 20:55
@@ -29,25 +24,5 @@ public class MyHashMap implements IMyHashMap {
         String value;
         Entry next;
 
-    }
-
-    public static void main(String[] args) {
-        DateFormat ft = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String dates="2020-11-03";
-        Date date=parseDate(dates,"yyyy-MM-dd");
-        Date newDate=new Date(date.getTime()+696842000/2);
-        System.out.println(ft.format(newDate));
-    }
-
-    public static Date parseDate(String val, String formats) {
-        String[] fs = formats.split(",");
-        for (String f : fs) {
-            DateFormat ft = new SimpleDateFormat(f);
-            try {
-                return ft.parse(val);
-            } catch (Exception e) {
-            }
-        }
-        return null;
     }
 }
