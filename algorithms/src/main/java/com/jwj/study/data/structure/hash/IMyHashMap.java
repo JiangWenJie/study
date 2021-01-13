9d69d372e02ff7e1a0f70e37cf97e45569bb9628
姜文杰 <wjjiang@che300.com>
2021-01-13 16:39:19 +0800

    feat:初始化hashMap接口

---
+++ algorithms/src/main/java/com/jwj/study/data/structure/hash/IMyHashMap.java
@@ -0,0 +1,12 @@
+package com.jwj.study.data.structure.hash;
+
+/**
+ * @author jwj
+ * @date 2021/1/6 18:06
+ * @description HashMap接口
+ * @since 1.0
+ */
+public interface IMyHashMap {
+    void put(String key,String value);
+    int get(String key);
+}
